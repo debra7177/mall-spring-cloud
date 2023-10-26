15f2d36fdc7e2964ac77c234372e3ae0b61e5626
Kevin <drzhong2015@gmail.com>
2023-10-27 03:42:44 +0800

    gateway 统一跨域

--- mall-gateway/src/main/java/org/eu/mall/gateway/MallGatewayApplication.java
+++ mall-gateway/src/main/java/org/eu/mall/gateway/MallGatewayApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+/**
+ * 1、开启服务注册发现
+ *  (配置nacos的注册中心地址)
+ * 2、编写网关配置文件
+ */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
 public class MallGatewayApplication {
---
+++ mall-gateway/src/main/java/org/eu/mall/gateway/config/MallCorsConfiguration.java
@@ -0,0 +1,27 @@
+package org.eu.mall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class MallCorsConfiguration {
+    // 跨域 Filter
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 设置跨域
+        //corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+
+    }
+}
