c678cf567659b1b76a169ef3993862c8585fd0f6
Kevin <drzhong2015@gmail.com>
2023-10-27 21:17:45 +0800

    第三方对象储存服务 服务端签名直传

---
+++ mall-third-party/src/test/java/org/eu/mall/thirdparty/MallThirdPartyApplicationTests.java
@@ -0,0 +1,48 @@
+package org.eu.mall.thirdparty;
+
+
+import com.aliyun.oss.OSSClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class MallThirdPartyApplicationTests {
+
+    @Autowired
+    private OSSClient ossClient;
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+//        // Endpoint以杭州为例，其它Region请按实际情况填写。
+//        String endpoint = "oss-cn-beijing.aliyuncs.com";
+//        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
+//        String accessKeyId = "LTAI4FwvfjSycd1APnuG9bjj";
+//        String accessKeySecret = "O6xaxyiWfSIitcOkSuK27ju4hXT5Hl";
+//
+//        // 创建OSSClient实例。
+//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        // 上传文件流。
+        InputStream inputStream = new FileInputStream("C:\\Users\\EDZ\\Pictures\\04d3afae928d1ca4.jpg");
+
+        ossClient.putObject("guanzhuo-vod-project", "hahaha.jpg", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+
+        System.out.println("上传完成...");
+    }
+
+}
