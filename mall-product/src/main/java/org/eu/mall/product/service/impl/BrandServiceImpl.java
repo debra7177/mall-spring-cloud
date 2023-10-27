e471fd1ff1ac59573ef237c0fb1f1ca0d6e3dca4
Kevin <drzhong2015@gmail.com>
2023-10-28 04:28:56 +0800

    更新品牌时 更新三级分类和品牌关联关系表的品牌信息

--- mall-product/src/main/java/org/eu/mall/product/service/impl/BrandServiceImpl.java
+++ mall-product/src/main/java/org/eu/mall/product/service/impl/BrandServiceImpl.java
@@ -1,21 +1,27 @@
 package org.eu.mall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.eu.common.utils.PageUtils;
 import org.eu.common.utils.Query;
-
 import org.eu.mall.product.dao.BrandDao;
 import org.eu.mall.product.entity.BrandEntity;
+import org.eu.mall.product.entity.CategoryBrandRelationEntity;
 import org.eu.mall.product.service.BrandService;
+import org.eu.mall.product.service.CategoryBrandRelationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+
+import java.util.Map;
 
 
 @Service("brandService")
 public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> implements BrandService {
+
+    @Autowired
+    private CategoryBrandRelationService categoryBrandRelationService;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -34,4 +40,14 @@ public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> impleme
         return new PageUtils(page);
     }
 
+    @Override
+    public void updateDetail(BrandEntity brand) {
+        // 保证冗余字段的数据一致
+        this.updateById(brand);
+        if (!StringUtils.isEmpty(brand.getName())) {
+            // 同步更新 category brand relation
+            categoryBrandRelationService.updateBrand(brand.getBrandId(), brand.getName());
+            // todo 更新其他关联
+        }
+    }
 }
