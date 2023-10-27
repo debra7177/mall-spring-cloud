c64b5fa1354b9302bbda582997aca926d2e2421d
Kevin <drzhong2015@gmail.com>
2023-10-28 01:06:32 +0800

    JSR303 参数校验

--- mall-product/src/main/java/org/eu/mall/product/entity/BrandEntity.java
+++ mall-product/src/main/java/org/eu/mall/product/entity/BrandEntity.java
@@ -6,10 +6,13 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.util.Date;
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.*;
 
 /**
  * 品牌
- * 
+ *
  * @author kevin
  * @email drzhong2015@gmail.com
  * @date 2023-10-27 00:34:58
@@ -27,10 +30,13 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 品牌名
 	 */
+	@NotBlank(message = "品牌名必须提交")
 	private String name;
 	/**
 	 * 品牌logo地址
 	 */
+	@NotEmpty
+	@URL(message = "logo必须是一个合法url")
 	private String logo;
 	/**
 	 * 介绍
@@ -43,10 +49,14 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 检索首字母
 	 */
+	@NotEmpty
+	@Pattern(regexp = "/^[a-zA-Z]$/", message = "检索字符必须是一个字母")
 	private String firstLetter;
 	/**
 	 * 排序
 	 */
+	@NotNull
+	@Min(value = 0, message = "排序必须大于等于0")
 	private Integer sort;
 
 }
---
+++ mall-product/src/main/java/org/eu/mall/product/exception/MallExceptionControllerAdvice.java
@@ -0,0 +1,35 @@
+package org.eu.mall.product.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.eu.common.exception.BizCodeEnum;
+import org.eu.common.utils.R;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+/*@ResponseBody
+@ControllerAdvice(basePackages = "org.eu.mall.product.controller")*/
+@RestControllerAdvice(basePackages = "org.eu.mall.product.controller")
+public class MallExceptionControllerAdvice {
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public R handleVaildException(MethodArgumentNotValidException e) {
+        log.error("数据校验出现问题{}, 异常类型:{}", e.getMessage(), e.getClass());
+        BindingResult bindingResult = e.getBindingResult();
+        Map<String, String> map = new HashMap<>();
+        bindingResult.getFieldErrors().forEach(fieldError -> {
+            map.put(fieldError.getField(), fieldError.getDefaultMessage());
+        });
+        return R.error(BizCodeEnum.VALID_EXCEPTION.getCode(), BizCodeEnum.VALID_EXCEPTION.getMsg()).put("data", map);
+    }
+
+    @ExceptionHandler(value = Throwable.class)
+    public R handleException(Throwable throwable) {
+        return R.error(BizCodeEnum.UNKNOWN_EXCEPTION.getCode(), BizCodeEnum.UNKNOWN_EXCEPTION.getMsg());
+    }
+}
