a90945754fe87dc86b707e502edd129681d4e87a
Kevin <drwangly2017@gmail.com>
2024-02-02 14:04:19 +0800

    第三方服务 发送手机验证码

---
+++ mall-third-party/src/main/java/org/eu/mall/thirdparty/component/SmsComponent.java
@@ -0,0 +1,58 @@
+package org.eu.mall.thirdparty.component;
+
+import lombok.Data;
+import org.apache.http.HttpResponse;
+import org.eu.mall.thirdparty.util.HttpUtils;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(prefix = "spring.cloud.alicloud.sms")
+@Data
+public class SmsComponent {
+
+    private String host;
+    private String path;
+    private String method ="POST";
+    private String appcode;
+    private String smsSignId;
+    private String templateId;
+
+    public void sendSmsCode(String phone, String code) {
+        String host = "https://dfsns.market.alicloudapi.com";
+        String path = "/data/send_sms";
+        String method = "POST";
+        Map<String, String> headers = new HashMap<>();
+        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+        headers.put("Authorization", "APPCODE " + appcode);
+        //根据API的要求，定义相对应的Content-Type
+        headers.put("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        Map<String, String> querys = new HashMap<>();
+        Map<String, String> bodys = new HashMap<>();
+        bodys.put("content", "code:" + code);
+        bodys.put("template_id", templateId);  //该模板为调试接口专用，短信下发有受限制，调试成功后请联系客服报备专属模板
+        bodys.put("phone_number", phone);
+
+        try {
+            /**
+             * 重要提示如下:
+             * HttpUtils请从
+             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java
+             * 下载
+             *
+             * 相应的依赖请参照
+             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
+             */
+            HttpResponse response = HttpUtils.doPost(host, path, method, headers, querys, bodys);
+            System.out.println(response.toString());
+            //获取response的body
+            //System.out.println(EntityUtils.toString(response.getEntity()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- mall-third-party/src/test/java/org/eu/mall/thirdparty/MallThirdPartyApplicationTests.java
+++ mall-third-party/src/test/java/org/eu/mall/thirdparty/MallThirdPartyApplicationTests.java
@@ -2,6 +2,8 @@ package org.eu.mall.thirdparty;
 
 
 import com.aliyun.oss.OSSClient;
+import org.apache.http.HttpResponse;
+import org.eu.mall.thirdparty.util.HttpUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -43,6 +49,49 @@ public class MallThirdPartyApplicationTests {
         ossClient.shutdown();
 
         System.out.println("上传完成...");
+    }
+
+    @Test
+    public void  test() {
+        String format = String.format("%06d", new BigInteger(UUID.randomUUID().toString().substring(0, 5), 16));
+        System.out.println(format);
+        System.out.println(format.length());
+    }
+
+    @Test
+    public void testSms() {
+        String host = "https://gyytz.market.alicloudapi.com";
+        String path = "/sms/smsSend";
+        String method = "POST";
+        String appcode = "ea16f2e9712c47a2b30466cba6fed2f9";
+        Map<String, String> headers = new HashMap<String, String>();
+        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+        headers.put("Authorization", "APPCODE " + appcode);
+        Map<String, String> querys = new HashMap<String, String>();
+        querys.put("mobile", "18871497906");
+        querys.put("param", "**code**:12345,**minute**:5");
+
+//smsSignId（短信前缀）和templateId（短信模板），可登录国阳云控制台自助申请。参考文档：http://help.guoyangyun.com/Problem/Qm.html
+
+        querys.put("smsSignId", "2e65b1bb3d054466b82f0c9d125465e2");
+        querys.put("templateId", "908e94ccf08b4476ba6c876d13f084ad");
+        Map<String, String> bodys = new HashMap<String, String>();
+
+        try {
+            /**
+             * 重要提示如下:
+             * HttpUtils请从\r\n\t    \t* https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java\r\n\t    \t* 下载
+             *
+             * 相应的依赖请参照
+             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
+             */
+            HttpResponse response = HttpUtils.doPost(host, path, method, headers, querys, bodys);
+            System.out.println(response.toString());
+            //获取response的body
+            //System.out.println(EntityUtils.toString(response.getEntity()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
