58c074e13bd930c6edcf850876703ded8be7b350
Kevin <drwangly2017@gmail.com>
2024-02-26 21:13:17 +0800

    mall购物车模块

---
+++ mall-cart/src/test/java/org/eu/mall/cart/MallCartApplicationTests.java
@@ -0,0 +1,17 @@
+package org.eu.mall.cart;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class MallCartApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
