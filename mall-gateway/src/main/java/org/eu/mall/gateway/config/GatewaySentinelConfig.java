d4474e7b8b0b814c7f6c76c98ef9b6b75a3e88e0
Kevin <drwangly2017@gmail.com>
2024-03-22 02:34:58 +0800

    alibaba-sentinel 定制网关流控返回

--- mall-gateway/src/main/java/org/eu/mall/gateway/config/GatewaySentinelConfig.java
+++ mall-gateway/src/main/java/org/eu/mall/gateway/config/GatewaySentinelConfig.java
@@ -1,10 +1,28 @@
 package org.eu.mall.gateway.config;
 
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
+import com.alibaba.fastjson.JSON;
+import org.eu.common.exception.BizCodeEnum;
+import org.eu.common.utils.R;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class GatewaySentinelConfig {
     public GatewaySentinelConfig() {
-
+        // todo 响应式编程
+        GatewayCallbackManager.setBlockHandler(new BlockRequestHandler() {
+            // 网关限流了请求 就会调用此回调 Mono Flux
+            @Override
+            public Mono<ServerResponse> handleRequest(ServerWebExchange serverWebExchange, Throwable throwable) {
+                R error = R.error(BizCodeEnum.TOO_MANY_REQUEST.getCode(), BizCodeEnum.TOO_MANY_REQUEST.getMsg());
+                String errorJson = JSON.toJSONString(error);
+                Mono<ServerResponse> body = ServerResponse.ok().body(Mono.just(errorJson), String.class);
+                return body;
+            }
+        });
     }
 }
